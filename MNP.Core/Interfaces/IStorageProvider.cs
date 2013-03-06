34035878403db596c1767aa064efe5ffe304a0e7
Stuart Blackler <stuart@sblackler.net>
2013-03-06 17:35:28 +0000

    Added basic interface methods, needs implementing

--- MNP.Core/Interfaces/IStorageProvider.cs
+++ MNP.Core/Interfaces/IStorageProvider.cs
@@ -1,9 +1,16 @@
-﻿
+﻿using System.IO;
+
 namespace MNP.Core
 {
     public interface IStorageProvider
     {
-        // TODO :: think of operations required
-        // read/write?
+        // For writing objects
+        void Write(string key, byte[] data);
+
+        // For the case of a log writter
+        void Write(string key, string str);
+
+        // For reading back from the data store
+        Stream Read(string key);
     }
 }
--- MNP.Server/Providers/Storage/DefaultStorageProvider.cs
+++ MNP.Server/Providers/Storage/DefaultStorageProvider.cs
@@ -4,6 +4,21 @@ namespace MNP.Server.Providers
 {
     public sealed class DefaultStorageProvider : IStorageProvider
     {
-        // TODO :: everything :(
+        // TODO :: implement interface 
+
+        public void Write(string key, byte[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(string key, string str)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.IO.Stream Read(string key)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
