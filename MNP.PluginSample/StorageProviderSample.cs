b0ba0aba224f13d2f77d09b1969d771a4e8d76db
Stuart Blackler <stuart@sblackler.net>
2013-03-06 17:40:30 +0000

    fixed build error in sample

--- MNP.PluginSample/StorageProviderSample.cs
+++ MNP.PluginSample/StorageProviderSample.cs
@@ -6,5 +6,19 @@ namespace MNP.PluginSample
     [Export(typeof(IStorageProvider))]
     public sealed class StorageProviderSample : IStorageProvider
     {
+        public void Write(string key, byte[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(string key, string str)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.IO.Stream Read(string key)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
